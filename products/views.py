08265652c7e3a4a8f1c8eb0e43cea244c5ba5baa
yurii <yuriydorosh2005@gmail.com>
2023-07-30 16:26:13 +0300

    UPDATE: title into views

--- products/views.py
+++ products/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Ara Shop',
+    }
+    return render(request, 'products/index.html', context)
+
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Ara Shop - Каталог',
+    }
+    return render(request, 'products/products.html', context)
