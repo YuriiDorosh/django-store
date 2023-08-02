f18c80fab4f629349d488ef4a8ffee7d6383a628
yurii <yuriydorosh2005@gmail.com>
2023-08-02 22:16:36 +0300

    UPDATE: forms customization

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,15 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': "Введіть ім'я користувача"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': "Введіть пароль"}))
+
     class Meta:
         model = User
         field = ('username', 'password')
