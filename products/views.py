fa5e11d96c58725c6f77cd9bcda8106f6938f7e2
Yurii <yuriydorosh2005@gmail.com>
2023-08-16 16:00:09 +0300

    UPDATE: pagination

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from products.models import Product, ProductCategory, Basket
-from users.models import User
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -13,12 +13,17 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(categories=category_id) if category_id else Product.objects.all()
+
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
 
     context = {
         'title': 'Ara Shop - Каталог',
         'categories': ProductCategory.objects.all(),
-        'products': Product.objects.filter(categories=category_id) if category_id else Product.objects.all(),
+        'products': products_paginator,
     }
     return render(request, 'products/products.html', context)
 
